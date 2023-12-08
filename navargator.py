922db8bd499208dc252bc1e3c6ef916bf182c2d6
dave-the-scientist <daves.binf.tools@gmail.com>
2023-12-08 15:35:25 -0500

    new machine test

--- navargator.py
+++ navargator.py
@@ -17,7 +17,7 @@ def new_random_port():
 
 
 # TODO:
-# - When I design the GUI-free version, ensure it makes good use of threading (brute force can split up the list of combinations; medoids+minibatch can split replicates; threshold might split up computation of cluster size?).
+# - When I design the GUI-free version, ensure it makes good use of threading (brute force can split up the list of combinations; medoids+minibatch can split replicates; threshold might split up computation of cluster size?). Threadpool should work.
 #   - Can even use the same command line option for threads. If using GUI it's the number of whole runs that can be processed at once; if not using GUI it's the number of threads a single run makes use of.
 # - Actually put this together with the option menus.
 #   - When done, edit the IE warning message at the top of input.html to include the correct command line flag.
